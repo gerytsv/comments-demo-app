678d3ebde8f90d348047dab31feb74cf8ef012c9
Gergana Tsvetkova <gerytsv@gmail.com>
2020-02-01 13:38:13 +0200

    Adding homepage and background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
+import { HomepageComponent } from './homepage/homepage.component';
+import { CommentModule } from './comment.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomepageComponent],
+  imports: [BrowserModule, AppRoutingModule, CommentModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
